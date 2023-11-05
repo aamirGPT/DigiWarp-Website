38c5229b6e6d58d4b3f62032e84eced0d4c5e80b
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2023-11-06 01:37:42 +0530

    background image

--- DigiWarp.js
+++ DigiWarp.js
@@ -6,6 +6,24 @@ document.getElementById("Menu-Toggle").addEventListener("click", function () {
     if (menuActions.classList.contains("active")) {
         menuToggle.textContent = "Close";
     } else {
-        menuToggle.innerHTML = '<img src="./assets/images/lines.png" alt="menu" id="HamburgerBtn"></img>';
+        menuToggle.innerHTML =
+            '<img src="./assets/images/lines.png" alt="menu" id="HamburgerBtn"></img>';
     }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".Card");
+
+    function checkVisibility() {
+        cards.forEach((card) => {
+            const rect = card.getBoundingClientRect();
+            const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+            if (isVisible) {
+                card.classList.add("animate-slide-in");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility(); // Check visibility on page load
 });
