faf305bd6584657e3d78cc0d7683e49442ac2b36
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2023-11-05 21:37:50 +0530

    Finalized Section 1

---
+++ DigiWarp.js
@@ -0,0 +1,12 @@
+document.getElementById("Menu-Toggle").addEventListener("click", function () {
+    const menuActions = document.querySelector(".Menu-Actions");
+    menuActions.classList.toggle("active");
+    document.getElementById("Menu-Toggle").style.flexDirection = "column";
+    const menuToggle = document.getElementById("Menu-Toggle");
+    if (menuActions.classList.contains("active")) {
+        menuToggle.textContent = "Close";
+    } else {
+        // menuToggle.textContent = "Menu";
+        menuToggle.innerHTML = '<img src="./assets/images/lines.png" alt="menu" id="HamburgerBtn"></img>';
+    }
+});
